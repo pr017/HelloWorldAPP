6bf23db70e257b6a629997329192fdfe9426def3
pr017 <53834073+pr017@users.noreply.github.com>
2019-11-15 03:37:07 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,9 @@ package com.company;
 public class Main
     {
         public static void main(String[] args)
-        { System.out.println("Hello World!");
+        { 
+            System.out.println("Hello World!");
+            System.out.println("zweite Ausgabezeile");
         }
 
     }
